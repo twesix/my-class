fcd53c605e0a2e680f386ec8b9b4874f8381248a
twesix <twesix@qq.com>
2019-02-26 14:01:40 +0800

    从数据库中取出所有数据

--- backend/accept_data.js
+++ backend/accept_data.js
@@ -9,11 +9,23 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.post('/', function(req, res)
+app.post('/', async function(req, res)
 {
     res.setHeader('Access-Control-Allow-Origin', '*')
     console.log(req.body)
     res.json(req.body)
+})
+
+app.get('/get_all_data', async function(req, res)
+{
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    const $db = await db.getDB()
+    $db.collection('student_submit').find({}).toArray(function(err, docs)
+    {
+        console.log(docs)
+        res.json(docs)
+    })
 })
 
 app.listen(port, function()
